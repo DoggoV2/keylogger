f88b8c547389f50472f957a99244dc5100a3d2d1
DoggoV2 <72258939+DoggoV2@users.noreply.github.com>
2021-01-03 15:19:33 +0100

    Update client.py

--- client.py
+++ client.py
@@ -4,7 +4,7 @@ socket_family = socket.AF_INET
 socket_type = socket.SOCK_STREAM
 
 server = socket.socket(socket_family, socket_type, proto=0)
-host = '178.135.1.229'
+host = '127.0.0.1'
 port = 54321
 server.connect((host, port))
 
